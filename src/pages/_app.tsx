19818eb2966e7214acc65b7f9f200e8c75abce68
ayan <ayan.websleak@gmail.com>
2026-01-12 22:45:03 +0530

    viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,15 @@ import "@/styles/globals.css";
 import "@/styles/font/Troylinesans/stylesheet.css";
 import "@/styles/font/Weissenhof/stylesheet.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
